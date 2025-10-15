2bb9494f4af456223dda87903d34a4d7e7d284f6
Rohit Rode <roderohit071@gmail.com>
2025-10-15 16:05:20 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://svasthya.vercel.app/",
     credentials: true,
 }));
 app.use(express.json());
